d98bd713082d0530dc0cd653d0cd9ba33209f336
Yashasvi <kmryashasvi@gmail.com>
2023-07-18 10:54:17 +0530

    added shadow in projects

--- app.js
+++ app.js
@@ -53,25 +53,3 @@ function zoomoInd()
     sal.style.backgroundColor='rgba(0, 0, 0, 0.3)';
     
 }
-
-// window.onload = function() {
-
-//   function isElementAbove(element1, element2) {
-//     const rect1 = element1.getBoundingClientRect();
-//     const rect2 = element2.getBoundingClientRect();
-  
-//     return rect1.top < rect2.top;
-//   }
-  
-//   const element1 = document.querySelector('.firstrow');
-//   const element2 = document.querySelector('.secrow');
-//   // Call the function
-//   const isAbove = isElementAbove(element1, element2);
-//   if(isAbove){
-//     const elements = document.querySelectorAll('.secrow');
-  
-//     elements.forEach((element) => {
-//       element.style.transform = `translate(${15}%, 0px)`;
-//     });
-//   }
-// };
